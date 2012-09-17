5abe634cb4ef5683a82251265a4a05db33a5453b
Madhok Shivaratre <gsmadhok@gmail.com>
2012-09-17 12:19:41 -0700

    Make a basuc user Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModellingUsers::Application.config.secret_token = '02f57c400bc199e584e96b15f35256973c606075e2a77de1f4f3de64af162d2f0d9f44f9de5886eaccb16828d32bb712d2477d6090457bfaf486413e836a97c6'
